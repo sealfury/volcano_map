f0a42caebe74071c7cd5fd722fda3f4fef175ee8
sealfury <willspecker@gmail.com>
2021-02-18 20:14:23 +0100

    incorporates geojson data for multipolygon display

--- map1.py
+++ map1.py
@@ -7,19 +7,22 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+
 def colorize(elevation):
     if elevation < 1200:
-        return 'green'
+        return "green"
     elif elevation > 2800:
-        return 'red'
+        return "red"
     else:
-        return 'orange'
+        return "orange"
+
 
 html = """<h4>Volcano Information:</h4>
 Height: %s m 
 """
 
 map = folium.Map(location=stockholm, zoom_start=4, tiles="Stamen Watercolor")
+
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
@@ -28,11 +31,12 @@ for lt, ln, el in zip(lat, lon, elev):
         folium.Marker(
             location=[lt, ln],
             popup=folium.Popup(iframe),
-            icon=folium.Icon(
-                color=colorize(el), icon="fa-fire", prefix="fa"
-            ),
+            icon=folium.Icon(color=colorize(el), icon="fa-fire", prefix="fa"),
         )
     )
-map.add_child(fg)
+fg.add_child(
+    folium.GeoJson(data=(open("data/world.json", "r", encoding="utf-8-sig").read()))
+)
 
+map.add_child(fg)
 map.save("Map1.html")
