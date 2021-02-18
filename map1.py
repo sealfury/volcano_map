827523a611aebde0a120bd3e424509d1941791e5
sealfury <willspecker@gmail.com>
2021-02-18 19:46:06 +0100

    iterates elevation data to display in marker popup, regenerates html

--- map1.py
+++ map1.py
@@ -4,17 +4,18 @@ import pandas
 data = pandas.read_csv("data/Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 stockholm = [59.33258, 18.0649]
 
 map = folium.Map(location=stockholm, zoom_start=4, tiles="Stamen Watercolor")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
+for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(
         folium.Marker(
             location=[lt, ln],
-            popup="Interested in volcanoes?",
+            popup=folium.Popup(str(el) + " m", parse_html=True),
             icon=folium.Icon(
                 color="red", icon_color="yellow", icon="fa-fire", prefix="fa"
             ),
