6fa111410b9bc509edde7eb0017ca9e6413ce1c1
sealfury <willspecker@gmail.com>
2021-02-18 18:33:23 +0100

    adds volcano info data file maps over latitude and longitude to generate markers for volcano locations regenerates html with updated functionality

--- map1.py
+++ map1.py
@@ -1,13 +1,25 @@
 import folium
+import pandas
 
-rejk = [64.1477, -21.93274]
-map = folium.Map(location=rejk, zoom_start=4, tiles="Stamen Watercolor")
+data = pandas.read_csv("data/Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+stockholm = [59.33258, 18.0649]
 
+map = folium.Map(location=stockholm, zoom_start=4, tiles="Stamen Watercolor")
 
-map.add_child(
-    folium.Marker(
-        location=rejk, popup="Interested in volcanoes?", icon=folium.Icon(color="green")
+fg = folium.FeatureGroup(name="My Map")
+
+for lt, ln in zip(lat, lon):
+    fg.add_child(
+        folium.Marker(
+            location=[lt, ln],
+            popup="Interested in volcanoes?",
+            icon=folium.Icon(
+                color="red", icon_color="yellow", icon="fa-fire", prefix="fa"
+            ),
+        )
     )
-)
+map.add_child(fg)
 
 map.save("Map1.html")
