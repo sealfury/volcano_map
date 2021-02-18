227add83bccb5185e0f2f41b2df37e5771a07462
sealfury <willspecker@gmail.com>
2021-02-18 18:08:36 +0100

    adds basic features to map

--- map1.py
+++ map1.py
@@ -1,3 +1,13 @@
-import folium 
-map = folium.Map(location=[64.1477, -21.93274], zoom_start=4)
+import folium
+
+rejk = [64.1477, -21.93274]
+map = folium.Map(location=rejk, zoom_start=4, tiles="Stamen Watercolor")
+
+
+map.add_child(
+    folium.Marker(
+        location=rejk, popup="Interested in volcanoes?", icon=folium.Icon(color="green")
+    )
+)
+
 map.save("Map1.html")
