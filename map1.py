14948e41755f695ee39e993611529e8b00586648
sealfury <willspecker@gmail.com>
2021-02-18 19:50:26 +0100

    adds html iframe styling to popup & regenerates html

--- map1.py
+++ map1.py
@@ -7,15 +7,19 @@ lon = list(data["LON"])
 elev = list(data["ELEV"])
 stockholm = [59.33258, 18.0649]
 
-map = folium.Map(location=stockholm, zoom_start=4, tiles="Stamen Watercolor")
+html = """<h4>Volcano Information:</h4>
+Height: %s m 
+"""
 
+map = folium.Map(location=stockholm, zoom_start=4, tiles="Stamen Watercolor")
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
     fg.add_child(
         folium.Marker(
             location=[lt, ln],
-            popup=folium.Popup(str(el) + " m", parse_html=True),
+            popup=folium.Popup(iframe),
             icon=folium.Icon(
                 color="red", icon_color="yellow", icon="fa-fire", prefix="fa"
             ),
